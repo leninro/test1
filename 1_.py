78aec2415725ba845964cf389103636fa8b23fe6
LEONTEV <leninro@yandex.ru>
2024-02-10 11:36:58 +0300

    test

---
+++ 1_.py
@@ -0,0 +1,6 @@
+a = 1 # a = int(1)
+b = 1.5 # b = float(1.5)
+c = 'str' # c = str ('CTO-TO')
+d = input('Insert any ')
+e = True # e = bool(True) False
+print(int(d) == False)
---
+++ calc.py
@@ -0,0 +1,20 @@
+first = int(input('Insert number'))
+oper = input('Insert logic')
+second = int(input('Insert number'))
+if oper == '+':
+    print(first+second)
+elif oper == '-':
+    print(first-second)
+elif oper == '*':
+    print(first*second)
+elif oper == '/':
+    print(first/second)
+elif oper == '**':
+    print(first**second)
+elif oper == '//':
+    print(first//second)
+elif oper == '%':
+    print(first%second)
+#LOL KEK cheburek
+    
+
